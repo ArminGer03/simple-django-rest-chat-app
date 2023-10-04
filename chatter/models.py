520833a047552b98ab5c453d1bc20373767670c3
ArminGer03 <a1382.geramirad@gmail.com>
2023-10-04 12:31:01 +0330

    Update models.py

--- chatter/models.py
+++ chatter/models.py
@@ -2,15 +2,17 @@ from django.contrib.auth.models import AbstractUser, models
 
 
 class CustomUser(AbstractUser):
-    # todo
     pass
 
 
 class Room(models.Model):
-    # todo
-    pass
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    members = models.ManyToManyField(CustomUser, on_delete=models.PROTECT)
 
 
 class Message(models.Model):
-    # todo
-    pass
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
