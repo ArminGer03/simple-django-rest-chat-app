53d743c5695b0673a955ed4d9c9d58fea778e16f
ArminGer03 <a1382.geramirad@gmail.com>
2023-10-10 16:26:50 +0330

    made an api for users

--- chatter/views.py
+++ chatter/views.py
@@ -1,4 +1,6 @@
 from rest_framework import mixins
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView
 from .serializers import UserSerializer
 
@@ -8,3 +10,15 @@ class RegisterUser(mixins.CreateModelMixin, GenericAPIView):
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+class UserMenu(APIView):
+    def get(self,request, id):
+         return Response(id)
+
+# class UserMenu(mixins.RetrieveModelMixin, mixins.CreateModelMixin, GenericAPIView):
+#     serializer_class = UserSerializer
+#     def get(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+#
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
