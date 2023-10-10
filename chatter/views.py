5abad8fe2420e19322afdfafba4cac4acd5c9111
ArminGer03 <a1382.geramirad@gmail.com>
2023-10-10 15:12:02 +0330

    changed to GenericAPIView

--- chatter/views.py
+++ chatter/views.py
@@ -1,13 +1,10 @@
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework.generics import GenericAPIView
 from .serializers import UserSerializer
 
 
-class RegisterUser(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        print(serializer.validated_data)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class RegisterUser(mixins.CreateModelMixin, GenericAPIView):
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
