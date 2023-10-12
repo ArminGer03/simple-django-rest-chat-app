5d618df7cd1432badd4d4cf9d95e7414795af5e4
ArminGer03 <a1382.geramirad@gmail.com>
2023-10-12 12:01:15 +0330

    changed URL from /user:<id>/ to /user/<id>/

--- chatter/urls.py
+++ chatter/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.RegisterUser.as_view(), name='register'),
-    path('user:<id>/', views.UserMenu.as_view(), name='user menu')
+    path('user/<id>/', views.UserMenu.as_view(), name='user menu')
     # path('login/', user_login, name='login'),
     # path('logout/', user_logout, name='logout'),
 ]
