78596f9299ae724f2699dae47ce0fe29dd1b68b0
ArminGer03 <a1382.geramirad@gmail.com>
2023-10-25 10:21:12 +0330

    added djoser package for authentication process

--- chatter/urls.py
+++ chatter/urls.py
@@ -1,20 +1,5 @@
-from django.urls import path
-
-# from rest_framework_nested import routers
+from django.urls import path, include
 from .views import RegisterUser, RoomMessages, UserMenu
-
-# /domain/ <- Domains list
-# /domain/{pk}/ <- One domain, from {pk}
-# /domain/{domain_pk}/nameservers/ <- Nameservers of domain from {domain_pk}
-# /domain/{domain_pk}/nameservers/{pk} <- Specific nameserver from {pk}, of domain from {domain_pk}
-
-# router = routers.SimpleRouter()
-# router.register(r'register', RegisterUser, basename='RegisterUser')
-#
-# domains_router = routers.NestedSimpleRouter(router, r'user', lookup='id')
-# domains_router.register(r'messages', RoomMessages, basename='RoomMessages')
-#
-# urlpatterns = router.urls + domains_router
 
 # 'basename' is optional. Needed only if the same viewset is registered more than once
 # Official DRF docs on this option: http://www.django-rest-framework.org/api-guide/routers/
@@ -35,6 +20,4 @@ urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register'),
     path('user/<id>/', UserMenu.as_view(), name='user_menu'),
     path('user/<id>/room/<room_pk>/', messages_list, name='room_messages'),
-    # path('login/', user_login, name='login'),
-    # path('logout/', user_logout, name='logout'),
 ]
--- djangorestProject/urls.py
+++ djangorestProject/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chatter.urls')),
+    path('auth/', include('djoser.urls'))
 ]
